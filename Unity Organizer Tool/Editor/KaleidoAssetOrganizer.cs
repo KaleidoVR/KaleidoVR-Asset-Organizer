2b5dd5c64174b81a3c82c910e502b85ff41040bc
KaleidoVR <KaleidoVR@hotmail.com>
2026-09-09 00:13:15 -0700

    Bump Asset Organizer to 1.0.5 for the Name Scene / Name Prefab samples.

--- Unity Organizer Tool/Editor/KaleidoAssetOrganizer.cs
+++ Unity Organizer Tool/Editor/KaleidoAssetOrganizer.cs
@@ -23,7 +23,7 @@ namespace KaleidoVR.EditorTools
 {
     public class KaleidoAssetOrganizer : EditorWindow
     {
-        public static readonly string VERSION = "1.0.4";
+        public static readonly string VERSION = "1.0.5";
         public const string LOGO_FILE_NAME = "Kali_Logo.png";
         public const string FALLBACK_ICON_PATH = "Assets/KaleidoVR/Editor/Icons/Kali_Logo.png";
 
