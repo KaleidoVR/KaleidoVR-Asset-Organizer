77a3c3e3ab8895251e95a6a14202beb8b22a1d0e
KaleidoVR <KaleidoVR@hotmail.com>
2026-09-08 23:29:47 -0700

    Fix LightShadowResolution compile error on 2022.3

--- Unity Organizer Tool/Editor/KaleidoAssetOrganizer.cs
+++ Unity Organizer Tool/Editor/KaleidoAssetOrganizer.cs
@@ -9,6 +9,7 @@
 // =========================================================================
 
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -22,7 +23,7 @@ namespace KaleidoVR.EditorTools
 {
     public class KaleidoAssetOrganizer : EditorWindow
     {
-        public static readonly string VERSION = "1.0.1";
+        public static readonly string VERSION = "1.0.2";
         public const string LOGO_FILE_NAME = "Kali_Logo.png";
         public const string FALLBACK_ICON_PATH = "Assets/KaleidoVR/Editor/Icons/Kali_Logo.png";
 
